5675c31390ae537917a0c3d380b7a39a46636781
Arzzzen <arzenslim@gmail.com>
2014-01-11 08:39:56 +0000

    initial commit

---
+++ src/Repository.php
@@ -0,0 +1,30 @@
+<?php
+  namespace Framework;
+
+  use Framework\pattern\IDataMFrameworker as IDataMFrameworker;
+
+  abstract class Repository implements IDataMFrameworker
+  {
+
+    private $model_name;
+
+    public function __construct() {
+      $this->model_name = str_replace('Repository', '', get_called_class());
+      if (!isset($this->table_name))
+      {
+        $this->table_name = $thsi->model_name;
+      }
+    }
+
+    public function find($id) {
+      $sth = DBManager::getInstance()->getDBH()->prepare(
+        'SELECT * FROM '.$this->table_name.' WHERE id=:id'
+      );
+      $sth->bindParam(':id', $id);
+      $sth->setFetchMode(\PDO::FETCH_INTO, new $this->model_name());
+      $sth->execute();
+      
+      return $sth->fetch();
+    }
+  }
+?>
